e0bddd91b5e80ed8fa089b22c49d2164c3102860
Морозов Дмитрий Андреевич ИП-514 <dmodmo0000@gmail.com>
2025-11-10 21:27:32 +0700

    Реализован метод add

--- Cache.java
+++ Cache.java
@@ -2,11 +2,21 @@ import java.util.LinkedList; // для LinkedList
 
 public class Cache<T> {
     private LinkedList<T> cacheList;
-    private int lastElements;
-
+    // n становиться хранителем последних элементов в тот момент когды
+    // в кэш добовляется какой либо новый элемент
+    private int lastElements; 
+    
     public Cache(int n) {
         this.cacheList = new LinkedList<>(); 
         this.lastElements = n;
+    }
+
+    public void add(T element) {
+        if (cacheList.size() >= lastElements) {
+            cacheList.removeFirst(); // удаляем самый старый элемент
+        } 
+
+        cacheList.addLast(element); // добавляем новый элемент в конец списка
     }
 
 
